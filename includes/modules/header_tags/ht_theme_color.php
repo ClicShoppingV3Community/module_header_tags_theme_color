6ef5cfd68c736f73c99fed06afeefb943d83a21a
ClicShopping <Yae8mr2o*>
2021-03-08 16:33:54 +0100

    update for 3.300 version

--- includes/modules/header_tags/ht_theme_color.php
+++ includes/modules/header_tags/ht_theme_color.php
@@ -15,8 +15,8 @@
 
   class ht_theme_color
   {
-    public string $code;
-    public string $group;
+    public $code;
+    public $group;
     public string $title;
     public string $description;
     public ?int $sort_order = 0;
